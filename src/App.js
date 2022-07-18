ff750a3b8357e8e658a58780956bf6f7b39f3b5e
Billy Otieno Okendo <billyotieno7@gmail.com>
2022-07-18 17:41:44 +0300

    updated App.js and Index.css

--- src/App.js
+++ src/App.js
@@ -13,24 +13,35 @@ const users = [
 ];
 
 // comment this out after completion and uncomment code below it to proceed
-function Child() {
-  return <div>This is children content</div>;
-}
-/**
-  Challenge: Uncomment this code to complete quiz
+// function Child() {
+//   return <div>This is children content</div>;
+// }
 
-function Child() {
+  //Challenge: Uncomment this code to complete quiz
+
+    function Child({ onValueUpdate }) {
   return (
     <>
-      <div>Child</div>
+      <div>Child
+
+
+        <h5>Amazing tech space</h5>
+      </div>
       <button>Change Parent Value</button>
+      <button onClick={onValueUpdate}>Change Parent Value</button>
     </>
   );
+
 }
+
+
+
+
 
 function Parent() {
   const [value, setValue] = React.useState(
     "I need to be updated from my child"
+
   );
 
   return (
@@ -47,29 +58,50 @@ function Parent() {
     </>
   );
 }
-Uncomment this to tackle quiz
-**/
+
+
+//Uncomment this to tackle quiz
+
 
 // Comment out after completion
-function Parent() {
-  return (
-    <div>
-      <h3>Parent Component</h3>
-    </div>
-  );
-}
+// function Parent() {
+//  return (
+//     <div>
+//       <h3>Parent Component</h3>
+//     </div>
+//   );
+
 // Comment above code after completion
 
 function App() {
-  const [] = React.useState(true);
+  
+  const [toggle, setToggle] = React.useState(true);
+
+  function toggleChange(){
+  
+    if (toggle===true){
+      setToggle(false);
+    }
+    else{
+      setToggle(true);
+    }
+
+
+  }
+
+  const userList=users.map((user) =>{
+    return <li key={user.id}>{user.name}</li>
+  })
   return (
     
     <>
-    
-    <img src={logo} className="App-logo" alt="logo" />
+    <h1>JSX is cool!</h1>
+    <img src="https://images.unsplash.com/photo-1564801720666-83b2d7a3de73?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c3RhcnJ5JTIwc2t5fGVufDB8fDB8fA%3D%3D&w=1000&q=80" className="App-logo" alt="logo" />
       <h3>User names</h3>
-      <ul></ul>
-      <button>Hide Element Below</button>
+      <ul>{userList}</ul>
+      <button onClick={toggleChange}>Hide Element Below</button>
+      <div>{toggle? "Toggle Challenge":" "}</div>
+      
 
       <div>Toggle Challenge</div>
       <Parent>
